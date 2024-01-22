7f030082035d327819eada5b6f2504a637b44649
Iñaki Armendariz <iniakitoo@gmail.com>
2024-01-22 18:37:54 -0300

    probando

--- backend/src/configs/db.ts
+++ backend/src/configs/db.ts
@@ -3,10 +3,16 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const { DB_DEPLOY } = process.env;
+const { DB_DEPLOY, DB_USER, DB_DATABASE, DB_PORT, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY!, {
-  dialect: 'postgres',
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+  logging: false,
+  native: false,
+  // dialectModule: pg,
 });
+
+// const sequelize = new Sequelize(DB_DEPLOY!, {
+//   dialect: 'postgres',
+// });
 
 export default sequelize;
